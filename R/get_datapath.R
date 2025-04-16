ed6e0fe2872b38d44e67920e564d3ffbf81a66dd
TeroJii <78660782+TeroJii@users.noreply.github.com>
2025-04-16 09:09:53 +0300

    add basic implementation of the function

--- R/get_datapath.R
+++ R/get_datapath.R
@@ -1,2 +1,20 @@
-get_datapath <- function(){
+get_datapath <- function(project_path = "", data_folder = "Data", use_dotenv = FALSE) {
+
+  stopifnot(is.character(project_path))
+  stopifnot(is.character(data_folder))
+
+  user_profile <- Sys.getenv("USERPROFILE")
+  if(use_dotenv){
+    project_path <- Sys.getenv("PROJECT_PATH")
+  }
+
+  # Get the path to the data directory
+  path <- file.path(user_profile, project_path, data_folder)
+
+  # Check if the path exists
+  if (!dir.exists(path)) {
+    stop(paste("The specified path does not exist:", path))
+  }
+
+  return(path)
 }
